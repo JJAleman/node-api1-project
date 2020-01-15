ba2d1244b9edf2d7abc007554c5c362475af8847
Juan Aleman <juan.j.aleman@gmail.com>
2020-01-14 23:59:47 -0600

    got server running

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 // implement your API here
-Will start my api 
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+server.listen(4000, () => {
+    console.log('\n **** server listening on port 4000 ****')
+});
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.send('Server works!');
+});
