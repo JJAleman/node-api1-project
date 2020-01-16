e1e5460d4851127f4f6f5896e4cb87ecb706c70e
Juan Aleman <juan.j.aleman@gmail.com>
2020-01-15 22:27:58 -0600

    GET request by id done

--- index.js
+++ index.js
@@ -57,3 +57,17 @@ server.post('/api/users', (req, res) => {
             res.status(500).json({success: false, errorMessage: "The users information could not be retrieved." })
         });
     });
+
+    server.get('/api/users/:id', (req, res) => {
+        db.findById(req.params.id)
+        .then(user =>{
+            if (user) {
+                res.status(200).json({ success: true, user});
+            } else {
+                res.status(404).json({ success: false, message: "The user with the specified ID does not exist." });
+            }
+        })
+        .catch( err => {
+            res.status(500).json({success: false, errorMessage: "The user information could not be retrieved."})
+        });
+    });
