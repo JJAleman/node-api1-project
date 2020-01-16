1e68603f8fe1fead6940187455f79436b1f05961
Juan Aleman <juan.j.aleman@gmail.com>
2020-01-15 23:09:53 -0600

    DELETE request implemented and working

--- index.js
+++ index.js
@@ -35,18 +35,10 @@ server.post('/api/users', (req, res) => {
         });
     };
 });
-    // db.insert(userInfo)
-    // .then(user => {
-    //     if (user) {
-    //         res.status(201).json({success: true, user});
-    //     } else {
-    //         res.status(400).json({success: false, errorMessage: "Please provide name and bio for the user."});
-    //     }
-    // })
     
-    // ----------------------
-    // GET request for users
-    // ------------------------
+// ----------------------
+// GET request for users
+// ------------------------
 
     server.get('/api/users', (req, res) => {
         db.find()
@@ -71,3 +63,22 @@ server.post('/api/users', (req, res) => {
             res.status(500).json({success: false, errorMessage: "The user information could not be retrieved."})
         });
     });
+
+// ------------------------
+// DELETE request
+// ------------------------
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+    .then(deleted => {
+        if(deleted) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({message: "The user with the specified ID does not exist."})
+        }
+    })
+    .catch( () => {
+        res.status(500).json({errorMessage: "The user could not be removed"});
+    });
+});
