58ed7d3bfbce34440f470f7c8ae7721ec1c540ea
Juan Aleman <juan.j.aleman@gmail.com>
2020-01-15 22:06:50 -0600

    POST completed needs fixing to check for an empty string

--- index.js
+++ index.js
@@ -11,7 +11,36 @@ server.listen(4000, () => {
 
 server.use(express.json());
 
-
+// Making sure server works (It does!) GET request
 server.get('/', (req, res) => {
     res.send('Server works!');
+})
+
+// -------------------------
+// POST
+// -------------------------
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+
+    if (userInfo.length === 0) {
+        res.status(400).json({success: false, errorMessage: "Please provide name and bio for the user."});
+    } else {
+        db.insert(userInfo)
+        .then( user => {
+            res.status(201).json({success: true, user});
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({errorMessage: "There was an error while saving the user to the database" });
+        });
+    };
 });
+    // db.insert(userInfo)
+    // .then(user => {
+    //     if (user) {
+    //         res.status(201).json({success: true, user});
+    //     } else {
+    //         res.status(400).json({success: false, errorMessage: "Please provide name and bio for the user."});
+    //     }
+    // })
+    
